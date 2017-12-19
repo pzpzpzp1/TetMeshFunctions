bb5e90da7f0c2c190d0cc6d755ca3e10c6ac64d1
pzpzpzp1 <pzpzpzp1@mit.edu>
2017-12-19 11:48:07 -0500

    fixed label/weights bug. it works now! without singularities.

--- PrimalSlice.m
+++ PrimalSlice.m
@@ -12,7 +12,7 @@ addpath(path2HMeshFuncs);
 % HMesh = LoadVTK(0, [path2HMeshFuncs '/meshes/FF/rod_ours.vtk']);
 % HMesh = LoadVTK(0, [path2HMeshFuncs '/meshes/FF/fertility.vtk']);
 % HMesh = LoadVTK(0, [path2HMeshFuncs '/meshes/FF/double_torus.vtk']);
-% HMesh = LoadVTK(0, [path2HMeshFuncs '/meshes/FF/ellipsoid-B.vtk']);
+HMesh = LoadVTK(0, [path2HMeshFuncs '/meshes/FF/ellipsoid-B.vtk']);
 
 TMesh = HexToTet(HMesh);
 X = TMesh.V2P;
@@ -20,7 +20,7 @@ T = TMesh.T2V;
 
 %[X,T]=paul_loadTetGenMesh('./meshes/sphere_61k');
 %[X,T]=paul_loadTetGenMesh('C:\Users\Administrator\Documents\jsolomon\octahedral_frames\meshes\sphere\spherer.1');
-[X,T]=paul_loadTetGenMesh('C:\Users\Administrator\Documents\jsolomon\octahedral_frames\meshes\sphere\spherer.1');
+%[X,T]=paul_loadTetGenMesh('C:\Users\Administrator\Documents\jsolomon\octahedral_frames\meshes\sphere\spherer.1');
 
 %data = paul_getTetData(T,X,1);
 data = paul_getTetData(T,X,0);
@@ -32,16 +32,18 @@ data = paul_getTetData(T,X,0);
 
 
 m = data.nonBoundaryTrianglesToTets;
-g = graph(m(:,1),m(:,2));
-g.Edges.Weights =  randi(100,size(data.nonBoundaryTrianglesToTets,1),1);
-g.Edges.Labels = (1:size(data.nonBoundaryTrianglesToTets,1))';
+g = graph(m(:,1),m(:,2),(1:size(data.nonBoundaryTrianglesToTets,1))');
+%g.Edges.Weights =  randi(100,size(data.nonBoundaryTrianglesToTets,1),1);
+%g.Edges.Labels = (1:size(data.nonBoundaryTrianglesToTets,1))';
 dualspantree = minspantree(g);
 nonBTrisInd = find(~data.isBoundaryTriangle);
-TriInds = nonBTrisInd(dualspantree.Edges.Labels);
+TriInds = nonBTrisInd(dualspantree.Edges.Weight);
 inTreeIndicator = sparse(TriInds, ones(size(TriInds,1),1), ones(size(TriInds,1),1), data.numTriangles, 1);
 
-%data.nonBoundaryTrianglesToTets(dualspantree.Edges.Labels(5),:)
-%data.trianglesToTets{nonBTrisInd(dualspantree.Edges.Labels(5))}
+%dualspantree.Edges.Labels(35,:)
+%dualspantree.Edges.EndNodes(35,:)
+%data.nonBoundaryTrianglesToTets(dualspantree.Edges.Labels(35),:)
+%data.trianglesToTets{nonBTrisInd(dualspantree.Edges.Labels(35))}'
 
 %{
 hold on;
@@ -49,7 +51,20 @@ dspv2v = dualspantree.Edges.EndNodes;
 for i = 1:size(dspv2v)
     tets = dspv2v(i,:);
     tetps = data.tetBarycenters(tets,:);
-    plot3(tetps(:,1),tetps(:,2),tetps(:,3));
+    plot3(tetps(:,1),tetps(:,2),tetps(:,3),'r','linewidth',2);
+    pause(.01)
+end
+%}
+
+%{
+hold on;
+dspv2v = find(inTreeIndicator);
+for i = 1:size(dspv2v)
+    tri = dspv2v(i,:);
+    tets = data.trianglesToTets{tri};
+    assert(numel(tets)==2);
+    tetps = data.tetBarycenters(tets,:);
+    plot3(tetps(:,1),tetps(:,2),tetps(:,3),'r','linewidth',2);
     pause(.01)
 end
 %}
@@ -59,26 +74,38 @@ totalTrianglesPerEdge = sum(e2tIndicator ,2);
 
 trianglesToClose = -1;
 while(numel(trianglesToClose)~=0)
-    closedTrianglesPerEdge = sum(e2tIndicator.*inTreeIndicator',2);
+    closedTrianglesPerEdge = e2tIndicator*inTreeIndicator;
     DualLoopsToClose = find((totalTrianglesPerEdge - closedTrianglesPerEdge)==1);
     trianglesToClose = find(sum(e2tIndicator(DualLoopsToClose, :),1)~=0);
+    
+    if(numel(intersect(find(inTreeIndicator),[164 210]))~=0)
+        1
+    end
+    
     inTreeIndicator(trianglesToClose) = 1;
 end
 
-% triangInds = 1:data.numTriangles;
-% intTriToTriIndex = triangInds(find(~data.isBoundaryTriangle));
-
-%tbs = data.triangleBarycenters(find(~inTreeIndicator),:);
-%scatter3(tbs(:,1),tbs(:,2),tbs(:,3),1);
-
 hold on;
-scatter3(data.vertices(find(data.isBoundaryVertex),1),data.vertices(find(data.isBoundaryVertex),2),data.vertices(find(data.isBoundaryVertex),3),1,'green')
+scatter3(data.vertices(find(data.isBoundaryVertex),1),data.vertices(find(data.isBoundaryVertex),2),data.vertices(find(data.isBoundaryVertex),3),5,'blue')
 surfaceTriInds = find(~inTreeIndicator);
+%surfaceTriInds = ARemoveB(surfaceTriInds',find(data.isBoundaryTriangle)');
 for i = 1:numel(surfaceTriInds)
     vinds = data.triangles(surfaceTriInds(i),:);
     polyPtch = data.vertices(vinds, :);
     ptc = patch(polyPtch(:,1), polyPtch(:,2), polyPtch(:,3),'green'); alpha(ptc, .05);
 end
 
+%{
+hold on;
+dspv2v = find(inTreeIndicator);
+for i = 1:size(dspv2v)
+    tri = dspv2v(i,:);
+    tets = data.trianglesToTets{tri};
+    assert(numel(tets)==2);
+    tetps = data.tetBarycenters(tets,:);
+    plot3(tetps(:,1),tetps(:,2),tetps(:,3),'r','linewidth',2);
+    pause(.01)
+end
+%}
 
 
